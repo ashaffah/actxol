16e8f8843027da5b05387f9e86f5b5f679009d8b
ashaffah <wagamama.morph@gmail.com>
2024-10-23 12:40:23 +0700

    refactor: add api to path route

--- src/main.rs
+++ src/main.rs
@@ -109,12 +109,26 @@ async fn streaming_response(path: web::Path<String>) -> HttpResponse {
 }
 
 /// Adds a new user to the "users" collection in the database.
-#[post("/add_user")]
+#[post("/api/add_user")]
 async fn add_user(cfg: web::Data<AppStates>, form: web::Form<User>) -> HttpResponse {
     let collection = cfg.db.collection("users");
     let result = collection.insert_one(form.into_inner()).await;
     match result {
         Ok(_) => HttpResponse::Ok().body("user added"),
+        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+    }
+}
+
+/// Gets the user with the supplied username.
+#[get("/api/get_user/{username}")]
+async fn get_user(cfg: web::Data<AppStates>, username: web::Path<String>) -> HttpResponse {
+    let username = username.into_inner();
+    let collection: Collection<User> = cfg.db.collection("users");
+    match collection.find_one(doc! { "username": &username }).await {
+        Ok(Some(user)) => HttpResponse::Ok().json(user),
+        Ok(None) => {
+            HttpResponse::NotFound().body(format!("No user found with username {username}"))
+        }
         Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
     }
 }
